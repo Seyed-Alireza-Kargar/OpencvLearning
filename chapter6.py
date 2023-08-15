59e5d9d651b0a8e95fa42cf3876a0183bcad97b8
Alireza <seyed.alireza.kargar1386@gmail.com>
2023-08-15 15:38:04 +0330

    chapter 6 not completed and not have descriptions

--- chapter6.py
+++ chapter6.py
@@ -2,11 +2,15 @@ import cv2
 import numpy as np
 
 img = cv2.imread('Resources\Lena.png')
-
 cv2.imshow('img', img)
 
-imgHor = np.hstack((img, img))
 
+imgHor = np.hstack((img, img))
 cv2.imshow("horizontal", imgHor)
+
+
+imgVer = np.vstack((img, img))
+cv2.imshow("Vertical", imgVer)
+
 
 cv2.waitKey(0)
